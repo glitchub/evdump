b9e15216a23d13e051e7d0c84d7d5e4765fd1462
glitchub <glitchub@users.noreply.github.com>
2019-07-26 16:47:27 -0700

    Support for mouse/mice devices

--- evdump.c
+++ evdump.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
@@ -30,6 +32,9 @@ Show events from up to 16 specified event devices.\n\
 \n\
 Device names can be specified as a full path, such as '/dev/input/event3', or\n\
 as 'event3', or just as '3'.\n\
+\n\
+Devices names containing the word 'mouse' or 'mice' are treated as three-octet\n\
+devices and generate virtual event streams.\n\
 ")
 
 const char *code(int t, int c)
@@ -50,15 +55,21 @@ const char *code(int t, int c)
     return NULL;
 }
 
+// report the given event
+void report(char *ename, struct timeval *etime, int etype, int ecode, int evalue)
+{
+    printf("%s %ld.%06ld type=%d (%s) code=%d (%s) value=%d\n", ename, etime->tv_sec, etime->tv_usec, etype, ed_EV(etype)?:"unknown", ecode, code(etype,ecode)?:"unknown", evalue);
+}
 
-int main(int argc, char* argv[])
+int main(int argc, char*argv[])
 {
     int ndevs;
     struct pollfd dev[MAXDEVS];
     char *name[MAXDEVS];
+    bool ismouse[MAXDEVS]; // true if the device generates three-octet event codes
 
     if (argc < 2) usage();
-    
+
     for (ndevs=0; ndevs < argc-1; ndevs++)
     {
         char *paths[] = {"%s", "/dev/%s", "/dev/input/%s", "/dev/input/event%s", NULL };
@@ -68,41 +79,69 @@ int main(int argc, char* argv[])
             if (!*p) die("Can't find event device '%s'\n", argv[ndevs+1]);
             asprintf(&name[ndevs], *p, argv[ndevs+1]);
             if ((dev[ndevs].fd = open(name[ndevs], O_RDWR))>=0)
-            { 
-                int version;
-                char dn[64];
-                struct input_id id;
+            {
+                ismouse[ndevs] = (strstr(name[ndevs],"mouse") || strstr(name[ndevs],"mice"));
 
-                if (ioctl(dev[ndevs].fd,EVIOCGVERSION,&version) == -1) die("%s EVIOCGVERSION failed: %s\n", name[ndevs], strerror(errno));
-                if (version != EV_VERSION) die("%s unsupported event version %d\n", name[ndevs], version);
-                if (ioctl(dev[ndevs].fd,EVIOCGNAME(sizeof dn),dn) == -1) die("%s: EVIOCGNAME failed: %s\n", name[ndevs], strerror(errno));
-                if (ioctl(dev[ndevs].fd,EVIOCGID,&id) == -1) die("%s EVIOCGID failed: %s\n", name[ndevs], strerror(errno));
-                
-                printf("%s name='%s' bus=%d (%s) vendor=%d product=%d version=%d\n", 
-                    name[ndevs], dn, id.bustype, ed_BUS(id.bustype)?:"unknown", id.vendor, id.product, id.version);
-
-                dev[ndevs].events=POLLIN;    
-                dev[ndevs].revents=0;    
+                if (!ismouse[ndevs])
+                {
+                    int version;
+                    char dn[64];
+                    struct input_id id;
+                    if (ioctl(dev[ndevs].fd,EVIOCGVERSION,&version) == -1) die("%s EVIOCGVERSION failed: %s\n", name[ndevs], strerror(errno));
+                    if (version != EV_VERSION) die("%s unsupported event version %d\n", name[ndevs], version);
+                    if (ioctl(dev[ndevs].fd,EVIOCGNAME(sizeof dn),dn) == -1) die("%s: EVIOCGNAME failed: %s\n", name[ndevs], strerror(errno));
+                    if (ioctl(dev[ndevs].fd,EVIOCGID,&id) == -1) die("%s EVIOCGID failed: %s\n", name[ndevs], strerror(errno));
+                    printf("%s name='%s' bus=%d (%s) vendor=%d product=%d version=%d\n",
+                        name[ndevs], dn, id.bustype, ed_BUS(id.bustype)?:"unknown", id.vendor, id.product, id.version);
+                }
+                else
+                {
+                    printf("%s name='mouse'\n", name[ndevs]);
+                }
+                dev[ndevs].events=POLLIN;
+                dev[ndevs].revents=0;
                 break;
             }
             if (errno==EACCES) die("%s permission denied (are you root?)\n",name[ndevs]);
             free(name[ndevs]);
-        }   
-    }    
+        }
+    }
 
     while (poll(dev, ndevs, -1) > 0)
     {
         for (int n=0; n < ndevs; n++)
         {
-            struct input_event e;
             if (dev[n].revents & POLLIN)
             {
-                if (read(dev[n].fd, &e, sizeof e) != sizeof e) die("%s read failed: %s\n", name[n], strerror(errno));
-                printf("%s %ld.%06ld type=%d (%s) code=%d (%s) value=%d\n",
-                    name[n], e.time.tv_sec, e.time.tv_usec, e.type, ed_EV(e.type)?:"unknown", e.code, code(e.type,e.code)?:"unknown", e.value);
-            }        
+                if (ismouse[n])
+                {
+                    // Simulate relative mouse:
+                    //   EV_KEY, BTN_MOUSE|BTN_RIGHT|BTN_MIDDLE, 1 or 0 (for each key change)
+                    //   EV_REL, REL_X, value
+                    //   EV_REL, REL_Y, value
+                    //   EV_SYN, SYN_REPORT, 0
+                    static int8_t pressed=0;
+                    struct timeval t;
+                    int8_t bxy[3];
+                    if (read(dev[n].fd, bxy, 3) != 3) die("%s read failed: %s\n", name[n], strerror(errno));
+                    gettimeofday(&t, NULL);
+                    int8_t change=(bxy[0] ^ pressed);
+                    pressed=bxy[0];
+                    if (change & 1) report(name[n], &t, EV_KEY, BTN_MOUSE, (bxy[0] & 1) != 0);
+                    if (change & 2) report(name[n], &t, EV_KEY, BTN_RIGHT, (bxy[0] & 2) != 0);
+                    if (change & 4) report(name[n], &t, EV_KEY, BTN_MIDDLE, (bxy[0] & 4) != 0);
+                    if (bxy[1]) report(name[n], &t, EV_REL, REL_X, bxy[1]);
+                    if (bxy[2]) report(name[n], &t, EV_REL, REL_Y, -bxy[2]); // y is negated
+                    report(name[n], &t, EV_SYN, SYN_REPORT, 0);
+                } else
+                {
+                    struct input_event e;
+                    if (read(dev[n].fd, &e, sizeof e) != sizeof e) die("%s read failed: %s\n", name[n], strerror(errno));
+                    report(name[n], &e.time, e.type, e.code, e.value);
+                }
+            }
             dev[n].revents=0;
         }
-    }    
+    }
     die("Poll failed: %s\n", strerror(errno));
 }
