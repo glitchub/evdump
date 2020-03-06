8a3c2062b391c0209498f69279f0dbbe93ddcab7
glitchub <glitchub@users.noreply.github.com>
2020-03-06 14:47:44 -0800

    mip

--- evls.c
+++ evls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -13,16 +14,11 @@
 
 #define die(...) fprintf(stderr, __VA_ARGS__), exit(1)
 
+// true if bit b is set in byte array a
 #define set(a, b) (a[(b)/8] & (1 << ((b) & 7)))
 
 int main(void)
 {
-    printf("EVIOCGABS(0)     = 0x%lX\n", (unsigned long)EVIOCGABS(0));
-    printf("EVIOCGABS(15)    = 0x%lX\n", (unsigned long)EVIOCGABS(15));
-    printf("EVIOCGBIT(0,8)   = 0x%lX\n", (unsigned long)EVIOCGBIT(0,8));
-    printf("EVIOCGBIT(10,8)  = 0x%lX\n", (unsigned long)EVIOCGBIT(10,8));
-    printf("EVIOCGBIT(10,15) = 0x%lX\n", (unsigned long)EVIOCGBIT(10,15));
-
     for (int ev = 0; ev < 32; ev++)
     {
         char device[32];
@@ -34,52 +30,160 @@ int main(void)
             goto next;
         }
 
-        printf("%s\n", device);
+        printf("%s:\n", device);
 
         char name[64];
         if (ioctl(fd, EVIOCGNAME(sizeof name), name) < 0) *name = 0;
-        printf("  %s\n", *name ? name : "Name unknown");
+        printf("  Name \"%s\"\n", *name ? name : "Name unknown");
 
-        unsigned char capabilities[(EV_MAX+7)/8];
-        int got = ioctl(fd, EVIOCGBIT(0, sizeof capabilities), capabilities);
-        if (got != sizeof capabilities)
+        int version;
+        if (ioctl(fd, EVIOCGVERSION, &version))
         {
-            printf("  EVIOCGBIT(0) returned %d\n", got);
+            printf("  EVIOCGVERSION failed\n");
+            goto next;
+        }
+        else if (version != EV_VERSION)
+        {
+            printf("  Unexpected stack version %d\n", version);
+            goto next;
+        }
+
+        struct input_id id;
+        if (ioctl(fd, EVIOCGID, &id))
+        {
+            printf("  EVIOCGID failed\n");
+            goto next;
+        }
+        printf("  Vendor %d\n", id.vendor);
+        printf("  Product %d\n", id.product);
+        printf("  Version %d\n", id.version);
+        printf("  Bus type %d is %s\n", id.bustype, ed_BUS(id.bustype)?:"unknown");
+
+        uint8_t capabilities[(EV_MAX+7)/8];
+        if (ioctl(fd, EVIOCGBIT(0, sizeof capabilities), capabilities) != sizeof capabilities)
+        {
+            printf("  EVIOCGBIT(0) failed\n");
             goto next;
         }
 
         for (int bit = 1; bit < EV_MAX; bit++) // note, skip bit 0 = EV_SYN
-        if (set(capabilities,bit))
+        if (set(capabilities, bit))
         {
-            printf("  Capability %d is %s\n", bit, ed_EV(bit) ?: "unknown");
+            printf("  Capability %d is %s\n", bit, ed_EV(bit)?:"unknown");
             switch(bit)
             {
+                case EV_KEY:
+                {
+                    uint32_t repeat[2];
+                    if (!ioctl(fd, EVIOCGREP, &repeat))
+                    {
+                        printf("    Repeat delay %d mS\n", repeat[0]);
+                        printf("    Repeat period %d mS\n", repeat[1]);
+                    }
+                    // Don't list all supported keys, there are hundreds, just show active
+                    uint8_t status[(KEY_MAX+7)/8];
+                    if (ioctl(fd, EVIOCGKEY(sizeof(status)), status) == sizeof status)
+                        for (int bit=0; bit < KEY_MAX; bit++)
+                            if (set(status, bit))
+                                printf("    Active key %d is %s\n", bit, ed_KEY(bit)?:"unknown");
+                    break;
+                }
+                case EV_REL:
+                {
+                    uint8_t supported[(REL_MAX+7)/8];
+                    if (ioctl(fd, EVIOCGBIT(EV_REL, sizeof supported), supported) == sizeof supported)
+                        for (int bit=0; bit < REL_MAX; bit++)
+                            if (set(supported, bit))
+                                printf("    Relative event %d is %s\n", bit, ed_REL(bit)?:"unknown");
+                    break;
+                }
                 case EV_ABS:
                 {
-                    uint8_t axes[(ABS_MAX+7)/8];
-                    int got = ioctl(fd, EVIOCGBIT(EV_ABS, sizeof(axes)), axes);
-                    if (got != sizeof axes)
+                    uint8_t supported[(ABS_MAX+7)/8];
+                    if (ioctl(fd, EVIOCGBIT(EV_ABS, sizeof(supported)), supported) == sizeof supported)
+                        for (int bit=0; bit < ABS_MAX; bit++)
+                            if (set(supported, bit))
+                            {
+                                printf("    Axis %d is %s\n", bit, ed_ABS(bit)?:"unknown");
+                                struct input_absinfo i;
+                                if (ioctl(fd, EVIOCGABS(bit), &i))
+                                {
+                                    printf("      Value     : %d\n", i.value);
+                                    printf("      Minimum   : %d\n", i.minimum);
+                                    printf("      Maximum   : %d\n", i.maximum);
+                                    printf("      Fuzz      : %d\n", i.fuzz);
+                                    printf("      Flat      : %d\n", i.flat);
+                                    printf("      Resolution: %d\n", i.resolution);
+                                }
+                            }
+                    break;
+                }
+                case EV_MSC:
+                {
+                    uint8_t supported[(MSC_MAX+7)/8];
+                    if (ioctl(fd, EVIOCGBIT(EV_MSC, sizeof supported), supported) == sizeof supported)
+                        for (int bit=0; bit < MSC_MAX; bit++)
+                            if (set(supported, bit))
+                                printf("    Misc event %d is %s\n", bit, ed_MSC(bit)?:"unknown");
+                    break;
+                }
+                case EV_SW:
+                {
+                    uint8_t supported[(SW_MAX+7)/8];
+                    if (ioctl(fd, EVIOCGBIT(EV_SW, sizeof supported), supported) == sizeof supported)
                     {
-                        printf("EVIOCGBIT(EV_ABS) returned %d\n", got);
-                        break;
-                    }
-                    for (int bit=0; bit < ABS_MAX; bit++) if (set(axes,bit))
-                    {
-                        printf("    Axis %d is %s\n", bit, ed_ABS(bit)?:"unknown");
-                        struct input_absinfo i;
-                        if (ioctl(fd, EVIOCGABS(bit), &i))
-                        {
-                            printf("      EVIOCGABS failed: %s\n", strerror(errno));
-                            continue;
-                        }
-                        printf("      Value     : %d\n", i.value);
-                        printf("      Minimum   : %d\n", i.minimum);
-                        printf("      Maximum   : %d\n", i.maximum);
-                        printf("      Fuzz      : %d\n", i.fuzz);
-                        printf("      Flat      : %d\n", i.flat);
-                        printf("      Resolution: %d\n", i.resolution);
+                        uint8_t status[sizeof supported];
+                        bool valid=true;
+                        if (ioctl(fd, EVIOCGSW(sizeof(status)), status) != sizeof status) valid=false;
+                        for (int bit=0; bit < SW_MAX; bit++)
+                            if (set(supported, bit))
+                            {
+                                if (valid && set(status, bit))
+                                    printf("    Active switch %d is %s\n", bit, ed_SW(bit)?:"unknown");
+                                else
+                                    printf("    Switch %d is %s\n", bit, ed_SW(bit)?:"unknown");
+                            }
                     }
                 }
+                case EV_LED:
+                {
+                    uint8_t supported[(LED_MAX+7)/8];
+                    if (ioctl(fd, EVIOCGBIT(EV_LED, sizeof supported), supported) == sizeof supported)
+                    {
+                        bool valid=true;
+                        uint8_t status[sizeof supported];
+                        if (ioctl(fd, EVIOCGLED(sizeof(status)), status) != sizeof status) valid=false;
+                        for (int bit=0; bit < LED_MAX; bit++)
+                            if (set(supported, bit))
+                            {
+                                if (valid && set(status, bit))
+                                    printf("    Active LED %d is %s\n", bit, ed_LED(bit)?:"unknown");
+                                else
+                                    printf("    LED %d is %s\n", bit, ed_LED(bit)?:"unknown");
+                            }
+                    }
+                    break;
+                }
+                case EV_SND:
+                {
+                    uint8_t supported[(SND_MAX+7)/8];
+                    if (ioctl(fd, EVIOCGBIT(EV_SND, sizeof supported), supported) == sizeof supported)
+                    {
+                        bool valid=true;
+                        uint8_t status[sizeof supported];
+                        if (ioctl(fd, EVIOCGSND(sizeof(status)), status) != sizeof status) valid=false;
+                        for (int bit=0; bit < SND_MAX; bit++)
+                            if (set(supported, bit))
+                            {
+                                if (valid && set(status, bit))
+                                    printf("    Active sound %d is %s\n", bit, ed_SND(bit)?:"unknown");
+                                else
+                                    printf("    Sound %d is %s\n", bit, ed_SND(bit)?:"unknown");
+                            }
+                    }
+                    break;
+                }
+
             }
         }
         next: close(fd);
